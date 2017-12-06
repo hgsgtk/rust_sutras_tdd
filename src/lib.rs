830de6d0365067b95cbf5615dc1cb23b5cbd4c23
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-06 22:09:20 +0900

    Implement Franc struct & impl

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,30 @@ pub struct Dollar {
     amount: u32
 }
 
+pub struct Franc {
+    amount: u32
+}
+
 impl Dollar {
-    pub fn new(amount: u32) -> Dollar {
+    pub fn new (amount: u32) -> Dollar {
         Dollar { amount: amount }
     }
     pub fn times (&self, multiplier: u32) -> Dollar {
         Dollar { amount: self.amount * multiplier }
     }
     pub fn equals (&self, target: Dollar) -> bool {
+        self.amount == target.amount
+    }
+}
+
+impl Franc {
+    pub fn new (amount: u32) -> Franc {
+        Franc { amount: amount }
+    }
+    pub fn times (&self, multiplier: u32) -> Franc {
+        Franc {amount: self.amount * multiplier }
+    }
+    pub fn equals (&self, target: Franc) -> bool {
         self.amount == target.amount
     }
 }
@@ -28,5 +44,11 @@ mod tests {
     fn test_equality() {
         assert!(Dollar::new(5).equals(Dollar::new(5)));
         assert!(!Dollar::new(5).equals(Dollar::new(6)));
+    }
+    #[test]
+    fn test_franc_multiplication() {
+        let five = Franc::new(5);
+        assert!(Franc::new(10).equals(five.times(2)));
+        assert!(Franc::new(15).equals(five.times(3)));
     }
 }
