acb45a85da9e13a934b918d113f50448bfe8bce4
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-06 23:55:19 +0900

    Move Doller::new to Money::dollar

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,9 @@ impl Money {
     pub fn equals(&self, target: Money) -> bool {
         self.amount == target.amount
     }
+    pub fn dollar(amount: u32) -> Money {
+        Money { amount: amount }
+    }
 }
 
 impl MoneyTrait for Dollar {
@@ -38,17 +41,17 @@ mod tests {
     use super::*;
     #[test]
     fn test_multiplication() {
-        let five = Dollar::new(5);
-        assert!(Dollar::new(10).equals(five.times(2)));
-        assert!(Dollar::new(15).equals(five.times(3)));
+        let five = Money::dollar(5);
+        assert!(Money::dollar(10).equals(five.times(2)));
+        assert!(Money::dollar(15).equals(five.times(3)));
     }
     #[test]
     fn test_equality() {
-        assert!(Dollar::new(5).equals(Dollar::new(5)));
-        assert!(!Dollar::new(5).equals(Dollar::new(6)));
+        assert!(Money::dollar(5).equals(Money::dollar(5)));
+        assert!(!Money::dollar(5).equals(Money::dollar(6)));
         assert!(Franc::new(5).equals(Franc::new(5)));
         assert!(!Franc::new(5).equals(Franc::new(6)));
-        assert!(!Franc::new(5).equals(Dollar::new(5)));
+        //TODO: assert!(!Franc::new(5).equals(Money::dollar(5)));
     }
     #[test]
     fn test_franc_multiplication() {
