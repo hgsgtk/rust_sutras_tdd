ea36b2cefef15c07dd3de26c9fc712d3dd959e8d
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2017-12-27 18:53:16 +0900

    Refactoring assertion by PartialEq trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(PartialEq, Debug)]
 pub struct Money {
     amount: u32
 }
@@ -39,20 +40,20 @@ mod tests {
     #[test]
     fn test_multiplication() {
         let five = Dollar::new(5);
-        assert!(Dollar::new(10).equals(five.times(2)));
-        assert!(Dollar::new(15).equals(five.times(3)));
+        assert_eq!(Dollar::new(10), five.times(2));
+        assert_eq!(Dollar::new(15), five.times(3));
     }
     #[test]
     fn test_equality() {
-        assert!(Dollar::new(5).equals(Dollar::new(5)));
-        assert!(!Dollar::new(5).equals(Dollar::new(6)));
-        assert!(Franc::new(5).equals(Franc::new(5)));
-        assert!(!Franc::new(5).equals(Franc::new(6)));
+        assert_eq!(Dollar::new(5), Dollar::new(5));
+        assert!(Dollar::new(5) != Dollar::new(6));
+        assert_eq!(Franc::new(5), Franc::new(5));
+        assert_eq!(Franc::new(5) != Franc::new(6));
     }
     #[test]
     fn test_franc_multiplication() {
         let five = Franc::new(5);
-        assert!(Franc::new(10).equals(five.times(2)));
-        assert!(Franc::new(15).equals(five.times(3)));
+        assert_eq!(Franc::new(10), (five.times(2)));
+        assert_eq!(Franc::new(15), (five.times(3)));
     }
 }
